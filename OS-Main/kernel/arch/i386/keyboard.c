e8c16babd943472f0770a35bfe5be2cf9bc82e62
Marko Zhen <marko.zhen@n-dimension.com>
2014-10-07 18:37:48 -0400

    Fixed tab issue and added some keyboard comments

--- OS-Main/kernel/arch/i386/keyboard.c
+++ OS-Main/kernel/arch/i386/keyboard.c
@@ -40,7 +40,7 @@ unsigned char kbdus[128] =
 	'[',
 	']',
 	'\n',
-	'0',
+	'0', // Control
 	'a',
 	's',
 	'd',
@@ -53,7 +53,7 @@ unsigned char kbdus[128] =
 	';',
 	'\'',
 	'`',
-	0,
+	0, // Left shift
 	'\\',
 	'z',
 	'x',
@@ -65,33 +65,33 @@ unsigned char kbdus[128] =
 	',',
 	'.',
 	'/',
-	0,
+	0, // Right shift
 	'*',
-	0,
+	0, // Alt
 	' ',
-	0,
-	0,
+	0, // Caps-Lock
+	0, // F1
 	0, 0, 0, 0, 0, 0, 0, 0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
+	0, // F10
+	0, // Num Lock
+	0, // Scroll Lock
+	0, // Home Key
+	0, // Up Arrow
+	0, // Page up
 	'-',
-	0,
-	0,
-	0,
+	0, // Left arrow
+	0, 
+	0, // Right arrow
 	'+',
-	0,
-	0,
-	0,
-	0,
-	0,
-	0, 0, 0,
-	0,
-	0,
-	0
+	0, // End Key
+	0, // Down arrow
+	0, // Page down
+	0, // Insert Key
+	0, // Delete key
+	0, 0, 0, 
+	0, // F11
+	0, // F12
+	0 // All other keys are undefined
 };
 
 void keyboard_handler(struct regs *r)
@@ -104,7 +104,6 @@ void keyboard_handler(struct regs *r)
 	if (scancode & 0x80)
 	{
 		// Use for shift, control and whatnot
-
 	}
 	else
 	{
--- OS-Main/kernel/arch/i386/tty.c
+++ OS-Main/kernel/arch/i386/tty.c
@@ -114,6 +114,7 @@ void terminal_putchar(char c)
 					terminal_column = 0;
 					if ( ++terminal_row == VGA_HEIGHT ) terminal_scroll();
 				}
+				terminal_session++;
 			}
 			move_cursor(terminal_row, terminal_column);
 			break;
